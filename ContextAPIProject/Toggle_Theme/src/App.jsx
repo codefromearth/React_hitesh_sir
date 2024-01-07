c905d5031be41be62f863647f802cdc50e3a1434
Raushan <raushan.kumar@bayatree.com>
2024-01-07 14:59:03 +0530

    contextApi

---
+++ ContextAPIProject/Toggle_Theme/src/App.jsx
@@ -0,0 +1,15 @@
+import Login from './component/Login'
+import Profile from './component/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+    return(
+ <UserContextProvider>
+ <h1>React ContextApi</h1>
+     <Login/>
+     <Profile/>
+    </UserContextProvider>
+    )
+}
+
+export default App
